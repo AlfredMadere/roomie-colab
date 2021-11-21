f101ca833611aac7817b6173c538e7a7db053b4e
Alfred Madere <alfred@stevemadere.com>
2021-11-20 23:22:12 -0600

    pygame setup successfull

--- planet-game/index.py
+++ planet-game/index.py
@@ -1,1 +1,22 @@
-print('hello world')
+import pygame
+#from pygame.constants import HIDDEN
+
+WIDTH, HEIGHT = 900, 500
+WIN =  pygame.display.set_mode((WIDTH, HEIGHT))
+WHITE = (255, 0, 0)
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        WIN.fill(WHITE)
+        pygame.display.update()
+    pygame.quit()
+
+#if __name__ == "__index__":
+main()
+
+
+#pygame.draw.rect(pygame.Surface,pygame.Color.aliceblue,(395,0,10,10))
