2210aa9601b94400d034b5ddf6acc90d464e5d70
Miles Herrman <94080765+milesherrman@users.noreply.github.com>
2021-11-25 17:19:27 -0700

    Update main.py

--- planet-game/main.py
+++ planet-game/main.py
@@ -4,14 +4,15 @@ from planet import Planet
 import setup as SETUP
 
 pygame.init()
-pygame.font.init()
 
 timer_event = pygame.USEREVENT + 1
 pygame.time.set_timer(timer_event, math.floor(1000/SETUP.FPS))
-Planet.generatePlanets(4)
+Planet.generatePlanets(8)
+Planet.generatePlanetsThatWillCollide()
 
 def draw_window():
     SETUP.WIN.fill(SETUP.WHITE)
+    Planet.doCollisions()
     Planet.updatePositions()
     for planet in Planet.planets:
         planet.render(SETUP.WIN)
@@ -24,10 +25,12 @@ def main():
         clock.tick(SETUP.FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("should quit")
+                print("Quiting")
                 run = False
             elif event.type == timer_event:
                 draw_window()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                Planet.generatePlanetsThatWillCollide()
     pygame.display.quit()
     pygame.quit()
 
